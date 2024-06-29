29cf9af468b069b77cec94843e8f134b361a01d5
todor1 <todorowa_el@abv.bg>
2024-06-29 18:57:36 +0300

    ch3 ep19 end

--- 03_socketio101/chat.js
+++ 03_socketio101/chat.js
@@ -5,8 +5,9 @@ const socketio = require("socket.io");
 app.use(express.static(__dirname + "/public"));
 
 const expressServer = app.listen(8001);
-// const io = socketio(expressServer);
 
+// io = the server object in the docs
+// const io = socketio(expressServer);
 const io = socketio(expressServer, {
   cors: {
     origin: "*",
@@ -15,8 +16,9 @@ const io = socketio(expressServer, {
 
 io.on("connection", (socket) => {
   console.log(socket.id, "has connected.");
-  socket.emit("messageFromServer", { data: "Welcome to the socket server!" });
-  socket.on("messageFromClient", (dataFromClient) => {
+  // socket.emit("messageFromServer", { data: "Welcome to the socket server!" });
+  socket.on("newMessageToServer", (dataFromClient) => {
     console.log("Data:", dataFromClient);
+    io.emit("newMessageToClients", { text: dataFromClient.text });
   });
 });
