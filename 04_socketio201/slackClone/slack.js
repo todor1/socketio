08d17dcff2dd5488fd1dfeb7305aceea0dde760c
todor1 <todorowa_el@abv.bg>
2024-07-02 17:45:57 +0300

    ch4 ep27 end

--- 04_socketio201/slackClone/slack.js
+++ 04_socketio201/slackClone/slack.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const app = express();
-// require("socket.io") = Server in the docs (Server = constructor)
 const socketio = require("socket.io");
 
 app.use(express.static(__dirname + "/public"));
 
-const expressServer = app.listen(8001);
+const expressServer = app.listen(9000);
 
-// io = the server object in the docs
 const io = socketio(expressServer, {
   cors: {
     origin: "*",
   },
 });
 
-// io = actual server object in the docs (built upon Server constructor)
 io.on("connection", (socket) => {
-  console.log(socket.id, "has connected.");
-  socket.on("newMessageToServer", (dataFromClient) => {
-    console.log("Data:", dataFromClient);
-    io.emit("newMessageToClients", { text: dataFromClient.text });
+  socket.emit("welcome", "Welcome to the server!");
+  socket.on("clientConnect", (data) => {
+    console.log(socket.id, "has connected.");
   });
 });
