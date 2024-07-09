5d8d477dd02248ae90232d363c6287c29eb63abc
todor1 <todorowa_el@abv.bg>
2024-07-09 14:01:49 +0300

    ch4 ep30 end

--- 04_socketio201/slackClone/public/scripts.js
+++ 04_socketio201/slackClone/public/scripts.js
@@ -16,6 +16,28 @@ socket.on("nsList", (nsData) => {
   nsData.forEach((ns) => {
     // update the HTML with each namespace
     const nameSpacesDiv = document.querySelector(".namespaces");
-    nameSpacesDiv.innerHTML += `<div class="namespace" ns="${ns.name}"><img src="${ns.image}" alt="${ns.name}"></div>`;
+    // nameSpacesDiv.innerHTML += `<div class="namespace" ns="${ns.name}"><img src="${ns.image}" alt="${ns.name}"></div>`;
+    nameSpacesDiv.innerHTML += `<div class="namespace" ns="${ns.endpoint}"><img src="${ns.image}" alt="${ns.name}"></div>`;
   });
+
+  Array.from(document.getElementsByClassName("namespace")).forEach(
+    (element) => {
+      console.log(element);
+      element.addEventListener("click", (e) => {
+        const nsEndpoint = element.getAttribute("ns");
+        console.log(nsEndpoint);
+
+        const clickedNs = nsData.find((row) => row.endpoint === nsEndpoint);
+        const rooms = clickedNs.rooms;
+        // get the room list div
+        let roomList = document.querySelector(".room-list");
+        // clear it out
+        roomList.innerHTML = "";
+        // loop through each room and add it to the DOM
+        rooms.forEach((room) => {
+          roomList.innerHTML += ` <li><span class="glyphicon glyphicon-lock"></span>${room.roomTitle}</li>`;
+        });
+      });
+    }
+  );
 });
